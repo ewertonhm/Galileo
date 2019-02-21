b8395536433295eaf8de5e080e12856f1ff677c7
Ewerton Henrique Marschalk <ewertonmarschalk@gmail.com>
2019-02-20 21:52:40 -0300

    Update login.php

--- main/login.php
+++ main/login.php
@@ -7,5 +7,16 @@
  */
 
     require_once 'Controllers/WebsiteCompiler.php';
+    require_once 'Controllers/LoginController.php';
 
-    $website = new WebsiteCompiler('Galileo Login', 'Usuário', 'login');
+    $loginController = new LoginController();
+    if($loginController->isLogged()){
+        header('location: index.php');
+    } else {
+        $website = new WebsiteCompiler('Galileo Login', 'login');
+        if(isset($_POST['btn-login'])){
+            if($loginController->login($_POST['login'],$_POST['password'])){
+                header('location: index.php');
+            }
+        }
+    }
