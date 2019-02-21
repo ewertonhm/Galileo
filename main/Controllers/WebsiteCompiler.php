d568992048a2ed45cd2a1114966d8bd0b6da5bab
Ewerton Henrique Marschalk <ewertonmarschalk@gmail.com>
2019-02-20 21:52:27 -0300

    Update WebsiteCompiler.php

--- main/Controllers/WebsiteCompiler.php
+++ main/Controllers/WebsiteCompiler.php
@@ -7,20 +7,21 @@
  */
 
 require_once $_SERVER['DOCUMENT_ROOT'].'/Galileo/main/Views/Views.php';
+require_once $_SERVER['DOCUMENT_ROOT'].'/Galileo/main/Models/Usuario.php';
+
 class WebsiteCompiler
 {
-    public $pagename, $username;
+    public $pagename;
     private $views;
 
-    public function __construct($pagename, $username, $page){
+    public function __construct($pagename, $page){
         $this->views = new Views();
         $this->pagename = $pagename;
-        $this->username = $username;
 
         $this->views->header($this->pagename);
 
         if ($page == 'index'){
-            $this->views->navbar($this->username);
+            $this->views->navbar($this->getUsername());
             $this->views->indexContent();
         }
 
@@ -30,5 +31,19 @@ class WebsiteCompiler
 
         $this->views->footer();
     }
+
+    public function getUsername(){
+        if(!isset($_SESSION)){
+            session_start();
+        }
+        if($_SESSION['logado']){
+            $usuario = new Usuario($_SESSION['user_id']);
+            return $usuario->getLogin();
+        } else {
+            return 'ERROR::NOTLOGGEDIN';
+        }
+    }
+
+
 
 }
