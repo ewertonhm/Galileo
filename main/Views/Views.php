530f66c4c73037457d8a9f0b3f9f8f174ad6d30e
Ewerton Henrique Marschalk <ewertonmarschalk@gmail.com>
2019-02-20 21:57:28 -0300

    fix index not found

--- main/Views/Views.php
+++ main/Views/Views.php
@@ -54,7 +54,7 @@ TAG;
   <div class='collapse navbar-collapse' id='navbarSupportedContent'>
     <ul class='navbar-nav mr-auto'>
       <li class='nav-item active'>
-        <a class='nav-link' href='index.html'>Inicio<span class='sr-only'>(current)</span></a>
+        <a class='nav-link' href='index.php'>Inicio<span class='sr-only'>(current)</span></a>
       </li>
       <li class='nav-item'>
         <a class='nav-link' href='#'>Sobre</a>
@@ -123,7 +123,7 @@ TAG;
                     </a>
                 </div>
                 <div class='col-sm-6 col-md-5 col-lg-4 item'>
-                    <a href='listaclit.php'>
+                    <a href='listacli.php'>
                         <div data-bs-hover-animate='pulse' class='box'><i class='fa fa-users icon'></i>
                             <h3 class='name'>Listar clientes</h3>
                         </div>
