271ff9e4b51658774ef0fa4491282bc562c3e98e
Ewerton Henrique Marschalk <ewertonmarschalk@gmail.com>
2019-02-25 11:42:59 -0300

    Create EstadoTest.php

---
+++ main/Models/EstadoTest.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ewert
+ * Date: 25/02/2019
+ * Time: 11:29
+ */
+
+use PHPUnit\Framework\TestCase;
+require_once 'Estado.php';
+
+class EstadoTest extends TestCase
+{
+
+    public function testGetCodigoUf()
+    {
+        $e = new Estado(15);
+        $this->assertEquals(15, $e->getCodigoUf());
+    }
+
+    public function testGetNome()
+    {
+        $e = new Estado(15);
+        $this->assertEquals('Pará',$e->getNome());
+    }
+
+}
