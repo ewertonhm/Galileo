ee20a0cac9c6b5e45a301c7412df40e9571d3037
Ewerton Henrique Marschalk <ewertonmarschalk@gmail.com>
2019-02-25 12:49:34 -0300

    Create UsuarioTest.php

---
+++ main/Testes/UsuarioTest.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ewert
+ * Date: 25/02/2019
+ * Time: 11:49
+ */
+
+use PHPUnit\Framework\TestCase;
+
+class UsuarioTest extends TestCase
+{
+
+    public function testSetLogin()
+    {
+
+    }
+
+    public function testSetPassword()
+    {
+
+    }
+
+    public function testGetId()
+    {
+
+    }
+
+    public function testGetLogin()
+    {
+
+    }
+
+    public function testGetPassword()
+    {
+
+    }
+
+    public function testGetIdPessoa()
+    {
+
+    }
+}
