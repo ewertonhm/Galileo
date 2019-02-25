be64abacee52c19dcfbac2a60e7e47355f281f71
Ewerton Henrique Marschalk <ewertonmarschalk@gmail.com>
2019-02-25 11:42:09 -0300

    Create CidadeTest.php

---
+++ main/Models/CidadeTest.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ewert
+ * Date: 25/02/2019
+ * Time: 08:56
+ */
+
+use PHPUnit\Framework\TestCase;
+require_once 'Cidade.php';
+
+class CidadeTest extends TestCase
+{
+
+    public function testGetCodigoIbge()
+    {
+        $t = new Cidade(1500107);
+        $this->assertEquals(1500107,$t->getCodigoIbge());
+    }
+
+    public function testGetNome()
+    {
+        $t = new Cidade(1500107);
+        $this->assertEquals('Abaetetuba',$t->getNome());
+    }
+
+    public function testGetLatitude()
+    {
+        $t = new Cidade(1500107);
+        $this->assertEquals('-1.72183',$t->getLatitude());
+    }
+
+    public function testGetLongitude()
+    {
+        $t = new Cidade(1500107);
+        $this->assertEquals('-48.8788',$t->getLongitude());
+    }
+
+    public function testGetCapital(){
+        $t = new Cidade(1500107);
+        $this->assertFalse($t->getCapital());
+    }
+
+    public function testGetNomeEstado(){
+        $t = new Cidade(1500107);
+        $this->assertEquals('Pará',$t->getNomeEstado());
+    }
+}
