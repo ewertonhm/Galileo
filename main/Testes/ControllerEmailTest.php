232a5a79cff6ad4788dd5ac68368ad9aa365ca3a
Ewerton Henrique Marschalk <ewertonmarschalk@gmail.com>
2019-02-25 17:37:43 -0300

    Update ControllerEmailTest.php

--- main/Testes/ControllerEmailTest.php
+++ main/Testes/ControllerEmailTest.php
@@ -6,10 +6,23 @@
  * Time: 17:31
  */
 
+require_once __DIR__ . '\..\vendor\autoload.php';
+
 use App\Controllers\Email;
 use PHPUnit\Framework\TestCase;
 
 class ControllerEmailTest extends TestCase
 {
+    public function testEmailCliente(){
+        $e = new Email();
+        $e->emailCliente(1);
+        $this->assertIsArray($e->getEmails());
+    }
+
+    public function testEmailPessoa(){
+        $e = new Email();
+        $e->emailPessoa(1);
+        $this->assertIsArray($e->getEmails());
+    }
 
 }
