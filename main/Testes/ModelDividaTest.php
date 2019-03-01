689878e03e0ae8a0db9210068232a160d953f8b3
Ewerton Henrique Marschalk <ewertonmarschalk@gmail.com>
2019-02-28 21:51:05 -0300

    Create ModelDividaTest.php

---
+++ main/Testes/ModelDividaTest.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ewert
+ * Date: 28/02/2019
+ * Time: 21:27
+ */
+require_once __DIR__ . '\..\vendor\autoload.php';
+use App\Models\Divida;
+use PHPUnit\Framework\TestCase;
+
+class DividaTest extends TestCase
+{
+
+    public function testCriarDivida()
+    {
+        $d = new Divida();
+        $d->setVencimento('10/10/20');
+        $d->setValor(100);
+        $this->assertTrue($d->criarDivida(1));
+    }
+
+    public function testEditarDivida()
+    {
+        $d = new Divida(2);
+        $d->setValor(rand());
+        $this->assertTrue($d->editarDivida());
+    }
+
+    public function testDeletarDivida()
+    {
+        $d = new Divida();
+        $d->setVencimento('10/10/20');
+        $d->setValor(rand());
+        $d->criarDivida(1);
+        $this->assertTrue($d->deletarDivida($d->getIdDivida()),$d->getIdDivida().'Deletado');
+
+    }
+
+    public function test__construct()
+    {
+        $d = new Divida(3);
+        $this->assertEquals('2020-10-10',$d->getVencimento());
+    }
+}
